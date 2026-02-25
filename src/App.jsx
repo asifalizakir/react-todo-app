9005039c336d4c323011283dcbf87edf1ed6a254
AsifAli <asifalizakir@gmail.com>
2026-02-25 17:33:41 +0530

    Applying map and filter method on data state variable

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ return<>
   <input type="text" value={count} onChange={(e)=>setCount(e.target.value)}></input>
 <button onClick={add}>Add</button>
 <button onClick={()=>setData([])}>Clear</button>
+<ul>{data.map((item,index)=>{return <li>{item}<button onClick={()=>setData(data.filter((_,i)=>i!==index))}> <img src="/2316149-200.png" width="10"></img></button></li>})}</ul>
+  
+
   </>
 
 }
